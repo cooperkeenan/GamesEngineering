ee844562f685efdaa1573b592c77fda17a6b8305
cooperlkeenan <cooperlkeenan@gmail.com>
2024-10-03 14:13:41 +0100

    tidy up

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -3,6 +3,11 @@
 using namespace sf;
 using namespace std;
 
+// Declarations
+void Load();
+void Update(RenderWindow& window);
+void Render(RenderWindow& window);
+
 const Keyboard::Key controls[4] = {
     Keyboard::A,   // Player1 UP
     Keyboard::Z,   // Player1 Down
@@ -16,8 +21,27 @@ const int gameHeight = 600;
 const float paddleSpeed = 400.f;
 const float paddleOffsetWall = 10.f;
 
+Vector2f ballVelocity;
+bool isPlayer1Serving = false;
+const float initialVelocityX = -100.f;
+const float initialVelocityY = -60.f;
+const float velocityMultiplier = 1.1f;
+
 CircleShape ball;
 RectangleShape paddles[2];
+
+
+int main() {
+    RenderWindow window(VideoMode(gameWidth, gameHeight), "PONG");
+    Load();
+    while (window.isOpen()) {
+        window.clear();
+        Update(window);
+        Render(window);
+        window.display();
+    }
+    return 0;
+}
 
 void Load() {
     // Set size and origin of paddles
@@ -25,6 +49,8 @@ void Load() {
         p.setSize(paddleSize);
         p.setOrigin(paddleSize / 2.f);
     }
+
+    ballVelocity = { (isPlayer1Serving ? initialVelocityX : -initialVelocityX), initialVelocityY };
     // Set size and origin of ball
     ball.setRadius(ballRadius);
     ball.setOrigin(ballRadius, ballRadius); //Should be half the ball width and height
@@ -41,6 +67,9 @@ void Update(RenderWindow &window) {
     // Reset clock, recalculate deltatime
     static Clock clock;
     float dt = clock.restart().asSeconds();
+
+    ball.move(ballVelocity * dt);
+
     // check and consume events
     Event event;
     while (window.pollEvent(event)) {
@@ -56,15 +85,56 @@ void Update(RenderWindow &window) {
     }
 
     // handle paddle movement
-    float direction = 0.0f;
+    float direction1 = 0.0f;
     if (Keyboard::isKeyPressed(controls[0])) {
-        direction--;
+        direction1--;
     }
     if (Keyboard::isKeyPressed(controls[1])) {
-        direction++;
+        direction1++;
     }
-    paddles[0].move(Vector2f(0.f, direction * paddleSpeed * dt));
+    paddles[0].move(0.f, direction1 * paddleSpeed * dt);
+
+    float direction2 = 0.0f;
+    if (Keyboard::isKeyPressed(controls[2])) {
+        direction2--;
+    }
+    if (Keyboard::isKeyPressed(controls[3])) {
+        direction2++;
+    }
+    paddles[1].move(0.f, direction2 * paddleSpeed * dt);
+
+    // Check ball collision with walls
+    const float bx = ball.getPosition().x;
+    const float by = ball.getPosition().y;
+
+    if (by <= ballRadius || by >= gameHeight - ballRadius) {
+        ballVelocity.y = -ballVelocity.y; // Invert Y velocity
+    }
+
+    if (bx <= ballRadius) {
+        // Ball hits left wall
+        isPlayer1Serving = false; // Player 2 serves next
+        ballVelocity.x = -initialVelocityX; // Reset ball velocity
+        ball.setPosition(gameWidth / 2.f, gameHeight / 2.f);
+    } else if (bx >= gameWidth - ballRadius) {
+        // Ball hits right wall
+        isPlayer1Serving = true; // Player 1 serves next
+        ballVelocity.x = initialVelocityX; // Reset ball velocity
+        ball.setPosition(gameWidth / 2.f, gameHeight / 2.f);
+    }
+
+    // Check ball collision with paddles
+    if ((bx <= paddles[0].getPosition().x + paddleSize.x / 2.f && bx >= paddles[0].getPosition().x - paddleSize.x / 2.f) &&
+        (by >= paddles[0].getPosition().y - paddleSize.y / 2.f && by <= paddles[0].getPosition().y + paddleSize.y / 2.f)) {
+        ballVelocity.x = -ballVelocity.x * velocityMultiplier; // Bounce off left paddle
+    }
+
+    if ((bx >= paddles[1].getPosition().x - paddleSize.x / 2.f && bx <= paddles[1].getPosition().x + paddleSize.x / 2.f) &&
+        (by >= paddles[1].getPosition().y - paddleSize.y / 2.f && by <= paddles[1].getPosition().y + paddleSize.y / 2.f)) {
+        ballVelocity.x = -ballVelocity.x * velocityMultiplier; // Bounce off right paddle
+    }
 }
+
 
 
 void Render(RenderWindow &window) {
@@ -75,14 +145,4 @@ void Render(RenderWindow &window) {
 }
 
 
-int main() {
-    RenderWindow window(VideoMode(gameWidth, gameHeight), "PONG");
-    Load();
-    while (window.isOpen()) {
-        window.clear();
-        Update(window);
-        Render(window);
-        window.display();
-    }
-    return 0;
-}
+ssh-add ~/Users/cooperkeenan/Documents/GitHub/GamesEngineering/ssh_key

0c74fa16befe67fbe669949406df538c1212b502
cooperlkeenan <cooperlkeenan@gmail.com>
2024-10-16 17:52:36 +0100

    different sprites used

--- 2_invaders/InvaderManager.cpp
+++ 2_invaders/InvaderManager.cpp
@@ -17,17 +17,22 @@ void InvaderManager::GenerateInvaders() {
     // Calculate margins and gaps
     float leftMargin = 30.0f;  
     float rightMargin = 30.0f; 
+    float additionalSpacing = 20.0f; 
     float availableWidth = windowWidth - leftMargin - rightMargin;
     float xPosGap = availableWidth / (totalColumns - 1);  
-    float yPosGap = 20.0f;    
+    float yPosGap = 22.0f;    
     float yPosition = 35.0f;  // Starting y position for the first row
+
+    int vec1 = 0;
+    int vecArr[5] = {32,64,96,128};
+
 
     // Generate invaders in an 11x5 grid
     for (int row = 0; row < totalRows; row++) {
         for (int col = 0; col < totalColumns; col++) {
             float xPos = leftMargin + col * xPosGap;
-            Invader* inv = new Invader(sf::IntRect(sf::Vector2i(0, 0), sf::Vector2i(32, 32)), { xPos, yPosition });
-            inv->setScale(0.47f, 0.47f);  // Scale down the invader sprite
+            Invader* inv = new Invader(sf::IntRect(sf::Vector2i(vecArr[row], 0), sf::Vector2i(32, 32)), { xPos, yPosition });
+            inv->setScale(0.40f, 0.47f);  // Scale down the invader sprite
             ships.push_back(inv);
         }
         yPosition += yPosGap;  // Move to the next row
